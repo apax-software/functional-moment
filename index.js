aadd0e0b5368e03e99cd2264a089705d58b5932e
Matt Smith <msmith@apaxsoftware.com>
2014-07-03 13:31:15 -0400

    setup docco

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var moment = require('moment');
 var R = require('ramda');
 
+// Loops over object and returns new object with functions bound to the original object
 var demethodizeMap = R.mapObj.idx(function (fn, fnName, obj) {
     return R.invoker(fnName, obj);
 });
 
+// Pulls off all the moment methods
 var fm = demethodizeMap(moment.fn);
+// Pulls off all the moment.duration methods
 var fd = demethodizeMap(moment.duration.fn);
 
+// Export the new object
 module.exports = exports = R.mixin(fm, {duration: fd});
